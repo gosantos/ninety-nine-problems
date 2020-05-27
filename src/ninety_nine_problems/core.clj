96bdaec952e514589baeafa9c18b3e37f37d47f5
Guilherme <guilhermeoliveira564@gmail.com>
2020-05-27 12:10:31 +0200

    Fix problem 1 when passing an empty list

--- src/ninety_nine_problems/core.clj
+++ src/ninety_nine_problems/core.clj
@@ -1,2 +1,8 @@
 (ns ninety-nine-problems.core)
 
+(defn last-element-from-a-list [list]
+  (case (count list)
+    0 []
+    1 (nth list 0)
+    (last-element-from-a-list (rest list))
+    ))
--- test/ninety_nine_problems/core_test.clj
+++ test/ninety_nine_problems/core_test.clj
@@ -2,12 +2,7 @@
   (:require [clojure.test :refer :all]
             [ninety-nine-problems.core :refer :all]))
 
-(defn last-element-from-a-list [list]
-  (if (= 1 (count list))
-    (nth list 0)
-    (last-element-from-a-list (rest list))
-    ))
-
 (deftest last-element-from-a-list-test
   (is (= 4 (last-element-from-a-list [1 2 3 4])))
-  (is (= 1 (last-element-from-a-list [1]))))
+  (is (= 1 (last-element-from-a-list [1])))
+  (is (= [] (last-element-from-a-list []))))
